231cbf88ef3888526a48ba7148e20d66311ff4d7
htmluser18 <likithkumardr18@gmail.com>
2025-11-26 16:02:51 +0530

    feat : added weapon choice to hero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from character import Hero, Enemy
-from weapon import iron_sword
+from weapon import fists, iron_sword, short_bow
 import os
 import sys
 
@@ -7,7 +7,25 @@ def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 hero = Hero("Hero", 100)
-hero.equip(iron_sword)
+
+print("-----choose your hero weapon----- \n 1.Fists \n 2.Iron Sword \n 3.Short Bow ")
+choice = input( )
+
+if choice == "2":
+    hero.equip(iron_sword)
+
+elif choice == "1":
+    hero.equip(fists)
+
+elif choice == "3":
+    hero.equip(short_bow)
+
+else :
+    print("invalid choice ,Equipping Iron sword")
+    hero.equip(iron_sword)
+
+
+    
 enemy = Enemy("Enemy", 100)
 
 while True:
